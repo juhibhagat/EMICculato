e3d5577b0357b78b7121f858205ecf924ce4a888
juhibhagat <jkri0206@gmail.com>
2025-05-16 22:16:34 -0700

    add link and change cide

---
+++ public/emi.js
@@ -0,0 +1,21 @@
+// const principal =document.querySelector('#nLoan-amount-input');
+// const interst =document.querySelector('#Interest-rate-info');
+// const tenure =document.querySelector('#months-to-pay');
+
+// const calculate =  document.querySelector('#calculator'); 
+// const emi = document.querySelector('#emi');
+
+// function calculateEmi(){
+//     const p =principal.ariaValueMax;
+//     const r= interst.value/1200;
+//     const n=tenure.value;
+//     const emiValue =(p*r*(1*r) / ((1+r) ** n-1));
+//     emi.value =emiValue.toFixed(2);
+// }
+
+
+
+function randa(){
+    var bc = document.getElementById("Loan-amount-input").value;
+    console.log(bc)
+}
